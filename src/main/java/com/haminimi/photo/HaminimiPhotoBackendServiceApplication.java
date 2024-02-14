0ff10401730592524f264f28a0c6da07db61551e
ttokey <ttokey.lee@gmail.com>
2024-02-14 13:55:57 +0900

    init project

---
+++ src/main/java/com/haminimi/photo/HaminimiPhotoBackendServiceApplication.java
@@ -0,0 +1,13 @@
+package com.haminimi.photo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HaminimiPhotoBackendServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HaminimiPhotoBackendServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/haminimi/photo/HaminimiPhotoBackendServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.haminimi.photo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HaminimiPhotoBackendServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
